f03b3522bbe387fab78c34476d2f26ba97ac1993
emesefedev <emesefedev@gmail.com>
2025-04-30 13:21:41 +0200

    create index view

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 const villagerRouter = require("./routes/villagerRouter");
 
 app.use("/villagers", villagerRouter);
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/", (req, res) => {
+    res.render("index", { message: "Inventory Application" });
+});
 
 const PORT = process.env.PORT || 3000;
 
