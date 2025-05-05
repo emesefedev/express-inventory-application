fd5faa802fb2006cb1b1c37aa77d20a25ad848b9
emesefedev <emesefedev@gmail.com>
2025-05-05 20:40:15 +0200

    feat: add birthdays link

--- utilities/links.js
+++ utilities/links.js
@@ -4,6 +4,7 @@ const linksVillagers = [
   { href: "/villagers/species", text: "Species" },
   { href: "/villagers/genders", text: "Genders" },
   { href: "/villagers/personalities", text: "Personalities" },
+  { href: "/villagers/birthdays", text: "Birthdays" },
 ]
 
 module.exports = {
